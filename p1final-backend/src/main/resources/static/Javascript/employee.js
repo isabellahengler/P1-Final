19d4bb2714d77ec1c808965161d48983585c8ddd
Isabella  Hengler <isabella.hengler57@gmail.com>
2022-03-27 20:11:54 -0700

    SECOND

--- p1final-backend/src/main/resources/static/Javascript/employee.js
+++ p1final-backend/src/main/resources/static/Javascript/employee.js
@@ -1,115 +1,37 @@
-function viewTicket() {
+async function displayInfo() {
 	
-	//gets the inputs from the employee's entry and assigns them to variables'
-	//var username = document.getElementById("username").value;
+	let input = document.getElementById("userName").value;
 	
-	//var password = document.getElementById("password").value;
+	let response = await fetch(`users/${input}`);
 	
-	//let data = {username: username, password: password};
+	let userData = await response.json();
 	
-	console.log(data);
+	console.log(userData);
 	
-	fetch("/users/{authorId}/reimbursement", {
-		 method: "POST",
-  		headers: {'Content-Type': 'application/json'}, 
-  		body: JSON.stringify(data)
-	}).then(res => {
-		
-		if(res.status !== 200) {
-			
-			//if login attempt fails
-			//alert("Incorrect Login Credentials");
-		}
-		
-		else {
-			
-			// Redirecting to other page.
-			window.location = "employeeTickets.html"; 
-		}
-		
-	  console.log("Request complete! response:", res);
-	});
-};
+	document.getElementById("userId").value = userData.id;
+	
+	document.getElementById("userFirst").value = userData.firstName;
+	
+	document.getElementById("userLast").value = userData.lastName;
+	
+	document.getElementById("username").value = userData.username;
+	
+	document.getElementById("pass").value = userData.password;
+	
+	document.getElementById("userEmail").value = userData.email;
 
-function managerLogin() {
-	
-	 var manuser = document.getElementById("username").value;
-	
-	var manpass = document.getElementById("password").value;
-	
-	let data = {username: manuser, password: manpass};
-	
-	console.log(data);
-	
-	fetch("/users/login", {
-		method: "POST",
-		headers: {'Content-Type': 'application/json'}, 
-  		body: JSON.stringify(data)
-	}).then(res => {
-		
-		if(res.status !== 200) {
-			
-			//if login attempt fails
-			alert("Incorrect Login Credentials");
-		}
-		
-		else {
-			
-			// Redirecting to other page.
-			window.location = "managers.html"; 
-		}
-		
-	  console.log("Request complete! response:", res);
-	});
-	
 }
 
-function logOut() {
+function employeeInfo() {
+	
+	window.location="employeeInfo.html"; 
+}
+
+
+function signOut() {
 	
 	window.location = "index.html";
-}
-
-function rembReq() {
-	
-	window.location = "request.html";
-}
-
-function oldReq() {
-	
-	window.location = "oldRequests.html";
-}
-
-function viewInfo() {
-	
-	window.location = "empInfo.html";
-}
-
-
-function appDeny() {
-	
-	window.location = "approveDeny.html";
-}
-
-function allPend() {
-	
-	window.location = "allPending.html";
-}
-
-function viewAll() {
-	
-	window.location = "allRequests.html";
-}
-function viewEmp() {
-	
-	window.location = "allEmployees.html";
-}
-function viewID() {
-	
-	window.location = "reqById.html";
-}
-
-
-
+	}
 
 
 
--- p1final-backend/src/main/resources/static/Javascript/manager.js
+++ p1final-backend/src/main/resources/static/Javascript/manager.js
@@ -1,4 +1,60 @@
-function signOut() {
+async function employeeTable() {
 	
-	window.location = "index.html";
-	}
+	let response = await fetch('/users');
+	
+	let userList = await response.json();
+	
+	let tableBody = document.getElementById("tableBody");
+	
+	console.log(userList);
+
+	for(let index = 0; index < userList.length; index++) {
+		
+		let element = userList[index];
+		
+		tableBody.innerHTML +=
+		`
+		<tr>
+      
+        <td class = "text-center">${element.id}</td>
+        <td class = "text-center">${element.username}</td>
+        <td class = "text-center">${element.password}</td>
+        <td class = "text-center">${element.firstName}</td>
+        <td class = "text-center">${element.lastName}</td>
+        <td class = "text-center">${element.email}</td>
+        
+        </tr>`;
+}
+}
+
+async function reimbersementTable() {
+	
+	let response = await fetch('/reimbursement');
+	
+	let rembList = await response.json();
+	
+	let rembBody = document.getElementById("rembBody");
+	
+	console.log(rembList);
+
+	for(let index = 0; index < rembList.length; index++) {
+		
+		let element = rembList[index];
+		
+		rembBody.innerHTML +=
+		`
+		<tr>
+      
+        <td class = "text-center">${element.id}</td>
+        <td class = "text-center">${element.amount}</td>
+        <td class = "text-center">${element.description}</td>
+        <td class = "text-center">${element.author}</td>
+        <td class = "text-center">${element.submitted}</td>
+        <td class = "text-center">${element.resolved}</td>
+        <td class = "text-center">${element.resolver}</td>
+        <td class = "text-center">${element.status}</td>
+        <td class = "text-center">${element.type}</td>
+
+        </tr>`;
+}
+}
